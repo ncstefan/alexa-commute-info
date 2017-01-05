709c72037219a4c061516ad6fb0b74dc3610d7af
Nick <ncstefan@yahoo.com>
2017-01-05 13:17:02 -0500

    refactored yesIntent to use the session crtUser

--- yesIntent.js
+++ yesIntent.js
@@ -16,40 +16,30 @@ console.log("yesIntent()");
     switch(req.sessionAttributes.previousState) {
         case "confirmName":
             res.session("previousState", "getTraffic");
-            var name = req.slot('Name');
-            var crtName = req.sessionAttributes["crtUser"];
-            console.log("yesIntent() for: " + crtName);
+            var name = req.slot('Name');  //!!!!!! this won't work
 
-            //get the car|bus option
-            //var opt = req.slot('Options');
-            //if (!opt) opt = "car";*/
-            //var opt = "car";
+            var crtName = req.sessionAttributes["crtUser"];
+            console.log("Processing yesIntent() for: " + crtName);
 
             //ask again if name not found in the list
             if (app.dictionary.names.indexOf(crtName) == -1){
-                var prompt = "Your name does not appear in the portal. Please register your userID in the registration portal.";
-                res.say(prompt).shouldEndSession(false);
-                res.send();
+                //user record not found
+                var prompt = "Your name and your destination route are not registered. Please follow the instructions you received in the application card to register on the Commute Info's application portal.";
+                res.say(prompt).shouldEndSession(false).send();
 
                 //send card
                 res.card({
                     type: "Standard",
                     title: "Here is your userID", // this is not required for type Simple
                     text: "Copy this:\n" + req.userId + "\nVisit the registration portal here:\n" + "http://alexacommuteinforeg.us-east-1.elasticbeanstalk.com" + "\nFollow these steps:\n1. Copy and paste your userID into the portal to have access to our services\n2. Provide the names of everyone in your household along with their home address and their destination address\n3. Now you can start using our services"
-                });
-                var prompt = "Welcome, check your alexa app, you will be provided with a card and some steps to follow.";
-                res.say(prompt).shouldEndSession(true);
-                res.send();
+                });                
                 return true;
             }
 
-            //save the current user
-            //res.session("crtUser", req.slot('Name'));
-
-            //get commute duration for <name>
+            //retrieve commute duration for <name>
             commute_info.getLiveTraffic(crtName, function(duration) {
-                res.say("Hello " + crtName + "! Your commute by car " + String(Math.round(duration/60)) + " minutes.").shouldEndSession(true);
-                res.send();
+                res.say("Hello " + crtName + "! Your commute by car " + String(Math.round(duration/60)) + " minutes.");
+                res.shouldEndSession(true).send();
             });
 
             //return false immediately so alexa-app doesn't send the response
